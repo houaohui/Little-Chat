a373dddbe59a390cddbb8d8b07192ceeabe82a97
houaohui <3179286437@qq.com>
2023-12-02 09:05:27 +0800

    init commit

--- toolchain/host/lib/gcc/arm-buildroot-linux-gnueabi/8.4.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "./configure --prefix=/home/hah/linux/f1c2000/buildroot-2020.02.4/output/host --sysconfdir=/home/hah/linux/f1c2000/buildroot-2020.02.4/output/host/etc --enable-static --target=arm-buildroot-linux-gnueabi --with-sysroot=/home/hah/linux/f1c2000/buildroot-2020.02.4/output/host/arm-buildroot-linux-gnueabi/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/home/hah/linux/f1c2000/buildroot-2020.02.4/output/host --with-mpc=/home/hah/linux/f1c2000/buildroot-2020.02.4/output/host --with-mpfr=/home/hah/linux/f1c2000/buildroot-2020.02.4/output/host --with-pkgversion='Buildroot 2020.02.4' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --enable-tls --enable-threads --without-isl --without-cloog --with-float=soft --with-abi=aapcs-linux --with-cpu=arm926ej-s --with-float=soft --with-mode=arm --enable-languages=c,c++ --with-build-time-tools=/home/hah/linux/f1c2000/buildroot-2020.02.4/output/host/arm-buildroot-linux-gnueabi/bin --enable-shared --disable-libgomp";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "arm926ej-s" }, { "float", "soft" }, { "mode", "arm" }, { "tls", "gnu" } };
